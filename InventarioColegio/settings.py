287472512f9e648448a1f3bbdf29955f8491c0f7
danny <alex15_25@hotmail.com>
2021-03-24 14:36:06 -0500

    cors x5

--- InventarioColegio/settings.py
+++ InventarioColegio/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['inventarioolegiodanny.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,9 +53,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-CORS_ORIGIN_WHITELIST = (
-    'localhost:8000'
-)
 
 REST_FRAMEWORK = {
     
